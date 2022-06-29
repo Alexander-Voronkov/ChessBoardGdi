30f919dcf9b4947a0982d765fe112aa673e9dac7
Voro_jn42@student.itstep.org <Voro_jn42@student.itstep.org>
2022-06-29 17:22:39 +0200

    Добавьте файлы проекта.

---
+++ Chesss/ChessSquare.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chesss
+{
+    public partial class ChessSquare : UserControl
+    {
+        Color c;
+        public ChessSquare()
+        {
+            InitializeComponent();
+            Size = new Size(30, 30);
+        }
+        public void SetColor(Color c)
+        {
+            this.c = c;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.Clear(c);
+            g.Dispose();
+        }
+
+        private void ChessSquare_MouseEnter(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.SmoothingMode=System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.DrawRectangle(Pens.Tomato,0,0,Size.Width,Size.Height);
+            g.Dispose();
+        }
+
+        private void ChessSquare_MouseLeave(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.Clear(c);
+        }
+    }
+}
---
+++ Chesss/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chesss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawChessBoard();
+        }
+        private void DrawChessBoard()
+        {
+            Point p = new Point(30,30);
+            bool f = false;
+            char a = 'a';
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    ChessSquare cs = new ChessSquare();
+                    cs.Left = p.X;
+                    cs.Top = p.Y;
+                    if(f)
+                        cs.SetColor(Color.Black);
+                    else
+                        cs.SetColor(Color.White);
+                    this.Controls.Add(cs);
+                    p=new Point(p.X+new ChessSquare().Size.Width,p.Y);
+                    f = !f;
+                }
+                f = !f;
+                p = new Point(new ChessSquare().Size.Width, p.Y+ new ChessSquare().Size.Height);
+            }
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            char a = 'a';
+            PointF p2 = new Point(Convert.ToInt32(new ChessSquare().Size.Width-10 * 1.5), 20);
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i == 0)
+                    {
+                        Graphics g = e.Graphics;
+                        g.DrawString((a++).ToString(), new Font("Times New Roman", 10), Brushes.Black, p2.X+20, p2.Y-10);
+                        p2 = new PointF(p2.X + new ChessSquare().Size.Width, p2.Y);
+                    }
+                    else
+                    {
+                        Graphics g = e.Graphics;
+                        g.DrawString((j + 1).ToString(), new Font("Times New Roman", 10), Brushes.Black, p2.X, p2.Y-10);
+                        p2 = new PointF(p2.X, p2.Y + new ChessSquare().Size.Height);
+                    }
+                }
+                p2 = new PointF(new ChessSquare().Size.Width / 2, p2.Y + new ChessSquare().Size.Height);
+            }
+        }
+    }
+}
